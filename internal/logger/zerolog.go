19ab4b14d77660f223e8fd78b973205e549e8dda
Quang nd <quangnd@huongvietgroup.com>
2024-09-19 10:54:40 +0700

    tes

--- internal/logger/zerolog.go
+++ internal/logger/zerolog.go
@@ -1,14 +1,9 @@
 package logger
 
 import (
-	"os"
-
 	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 )
 
 func SetupZeroLogger() {
-	service_name := os.Getenv("service_name")
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	log.Logger = log.With().Str("service", service_name).Logger()
 }
